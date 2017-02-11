484c17ed99ada8d8d78978c70ee0186461be1b98
Ramesh Syangtan <rameshsyangtan92@gmail.com>
2017-02-12 00:17:09 +0545

    add members page

---
+++ members/members.js
@@ -0,0 +1,18 @@
+/* Member information */
+
+const members = [
+  // add here
+]
+
+/* ====================================================================== */
+const membersContainer = document.querySelector('.members')
+let membersElms = '' // members elements
+
+// generate html of members data
+
+members.forEach(member => {
+  const memElm = `<div class='member'><img class='member-image' src='../assets/img/members/${member.dicordUsername}.png' alt='${member.fullName}' title='${member.fullName}'><b>${member.fullName} @ <span title='discord username'>${member.dicordUsername} </span></b><hr><p>${member.about}</p><hr><div class='member-links'><a href='https://github.com/${member.links.github}'><i class='fa fa-github'></i></a><a href='https://twitter.com/${member.links.twitter}'><i class='fa fa-twitter'></i></a><a href='https://facebook.com/${member.links.facebook}'><i class='fa fa-facebook'></i></a></div></div>`
+  membersElms += memElm
+})
+
+membersContainer.innerHTML = membersElms
