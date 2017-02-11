f86e62b6616a4de8ee23a352c3e5656469afa10b
Ramesh Syangtan <rameshsyangtan92@gmail.com>
2017-02-12 00:17:09 +0545

    add members page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,29 @@
-//require('../css/style.css');
-(function() {
-	
-	const menuBar = document.querySelector(".menu-bar-icon");
-	const menus = document.querySelector('.nav-menus');
-	const rootElement = document.documentElement; // root Element
-
+(function () {
+  const menuBar = document.querySelector('.menu-bar-icon')
+  const menus = document.querySelector('.nav-menus')
+  const rootElement = document.documentElement // root Element
 
 	// shows current date next copyright
-	document.querySelector('.date').textContent = new Date().getFullYear();
+  document.querySelector('.date').textContent = new Date().getFullYear()
 
-	function navHeightOnScroll() {
-		const scrollAmt = window.scrollY;
-		if(scrollAmt > 100 && scrollAmt < 200) {
-			// set navbar height to 80 px 
-			rootElement.style.setProperty('--navbar-height', '80px');	
-		} else if (scrollAmt < 100) {
-			// set navbar height to 100px 
-			rootElement.style.setProperty('--navbar-height', '100px');
-		}
-		else 
-			return;
+  function navHeightOnScroll () {
+    const scrollAmt = window.scrollY
+    if (scrollAmt > 100 && scrollAmt < 200) {
+    // set navbar height to 80 px
+      rootElement.style.setProperty('--navbar-height', '80px')
+    } else if (scrollAmt < 100) {
+	  // set navbar height to 100px
+      rootElement.style.setProperty('--navbar-height', '100px')
+    } else {
+      return false
+    }
+  }
 
-	}
-
-	function navHide() {
-		menus.classList.toggle('nav-hide');
-	}
-		
+  function navHide () {
+    menus.classList.toggle('nav-hide')
+  }
+  
 	// Event Listeners
-	menuBar.addEventListener('click', navHide);
-	window.addEventListener('scroll', navHeightOnScroll);
-
-})();
+  menuBar.addEventListener('click', navHide)
+  window.addEventListener('scroll', navHeightOnScroll)
+})()
