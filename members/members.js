94ef54188a9a825a1b46f2b950a51470fe51a373
Animesh Risal <animeshrisal99@gmail.com>
2017-03-07 21:15:40 +0545

    Update members.js

--- members/members.js
+++ members/members.js
@@ -82,6 +82,18 @@ const members = [
       facebook: 'cdrrazanme'
     }
   },
+  
+  //Animesh Risal
+   {
+    fullName: 'Animesh Risal',
+    discordUsername: '!10Toasts',
+    about: 'Machine Learning, NLP, open education',
+    links: {
+      github: 'TenToasts',
+      twitter: 'TenToasts',
+      facebook: 'TenToasts'
+    }
+  },
 ]
 
 /* ====================================================================== */
