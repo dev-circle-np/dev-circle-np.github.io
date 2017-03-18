3ce0441b0efed102043924f702a9ad6f7eb7f436
Pratik Chaudhary <chaudharypratik97@gmail.com>
2017-03-18 16:21:46 +0545

    Add AbsoluteZero273 (#30)
    * Add AbsoluteZero273
    
    * Fix syntax error

--- members/members.js
+++ members/members.js
@@ -132,6 +132,16 @@ const members = [
       facebook: 'iampradp'
     }
   },
+  // Pratik Chaudhary  
+  {
+    fullName: 'Pratik Chaudhary',
+    discordUsername: 'AbsoluteZero',
+    about: 'Hacks FP',
+    links: {
+      github: 'AbsoluteZero273',
+      twitter: 'CodeZero273'
+    }
+  }
   // add here
 ]
 /* ====================================================================== */
