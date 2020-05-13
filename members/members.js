b7d964454f4b8412edb4bd475bd3a451123b14b4
Manjil Tamang <lamamanjil@gmail.com>
2020-05-13 16:03:52 +0545

    new member added - Manjil Tamang

--- members/members.js
+++ members/members.js
@@ -3,279 +3,288 @@
 const members = [
   // CodeGeass
   {
-    fullName: 'Prashant Shahi',
-    discordUsername: 'CodeGeass#2925',
-    about: 'DevRel | Full Stack DevOps Engineer | Hails from Jumla, Nepal',
+    fullName: "Prashant Shahi",
+    discordUsername: "CodeGeass#2925",
+    about: "DevRel | Full Stack DevOps Engineer | Hails from Jumla, Nepal",
     links: {
-      github: 'prashant-shahi',
-      twitter: 'coolboi567',
-      facebook: 'coolboi567',
+      github: "prashant-shahi",
+      twitter: "coolboi567",
+      facebook: "coolboi567"
     }
   },
   // puncoz
   {
-    fullName: 'Puncoz Nepal',
-    discordUsername: 'puncoz#7209',
-    about: 'Full Stack Software Engineer | NodeJS | PHP | AI | ML',
+    fullName: "Puncoz Nepal",
+    discordUsername: "puncoz#7209",
+    about: "Full Stack Software Engineer | NodeJS | PHP | AI | ML",
     links: {
-      github: 'puncoz',
-      twitter: 'PuncozNepal',
-      facebook: 'puncoz'
+      github: "puncoz",
+      twitter: "PuncozNepal",
+      facebook: "puncoz"
     }
   },
   // rameshsyn
   {
-    fullName: 'Ramesh Syangtan',
-    discordUsername: 'rameshsyn#6074',
-    about: 'I am teacher, developer & student from Suryabinayak, Bhaktapur. ',
+    fullName: "Ramesh Syangtan",
+    discordUsername: "rameshsyn#6074",
+    about: "I am teacher, developer & student from Suryabinayak, Bhaktapur. ",
     links: {
-      github: 'rameshsyn',
-      twitter: 'ramesh_syn',
-      facebook: 'rameshsyn'
+      github: "rameshsyn",
+      twitter: "ramesh_syn",
+      facebook: "rameshsyn"
     }
   },
   // ashish
   {
-    fullName: 'Ashish Acharya',
-    discordUsername: 'ashish#2010',
-    about: 'Developer | Designer | Student | Nepal',
+    fullName: "Ashish Acharya",
+    discordUsername: "ashish#2010",
+    about: "Developer | Designer | Student | Nepal",
     links: {
-      github: 'anarchyrucks',
-      twitter: 'anarchyrucks',
-      facebook: 'anarchyrucks'
+      github: "anarchyrucks",
+      twitter: "anarchyrucks",
+      facebook: "anarchyrucks"
     }
   },
-   // sandeep pokhrel
+  // sandeep pokhrel
   {
-    fullName: 'sandeep pokhrel',
-    discordUsername: 'sandeep pokhrel',
-    about: 'I am a front-end developer',
+    fullName: "sandeep pokhrel",
+    discordUsername: "sandeep pokhrel",
+    about: "I am a front-end developer",
     links: {
-      github: 'sananddev',
-      twitter: 'Sandeep60837075',
-      facebook: 'sandeep.pokhrel.7'
+      github: "sananddev",
+      twitter: "Sandeep60837075",
+      facebook: "sandeep.pokhrel.7"
     }
   },
   // bibek
   {
-    fullName: 'Bibek Lamsal',
-    discordUsername: 'bibek',
-    about: 'I am a Data Analyst and a budding Software Engineer.',
+    fullName: "Bibek Lamsal",
+    discordUsername: "bibek",
+    about: "I am a Data Analyst and a budding Software Engineer.",
     links: {
-      github: 'bibek94',
-      twitter: 'Bibek21516111',
-      facebook: 'bibek.lamsal.94'
+      github: "bibek94",
+      twitter: "Bibek21516111",
+      facebook: "bibek.lamsal.94"
     }
   },
   // nirmalrizal
   {
-    fullName: 'Nirmal Rijal',
-    discordUsername: 'nirmalrizal#8173',
-    about: 'Craze for Machine Learning and Data Science :)',
+    fullName: "Nirmal Rijal",
+    discordUsername: "nirmalrizal#8173",
+    about: "Craze for Machine Learning and Data Science :)",
     links: {
-      github: 'nirmalrizal53',
-      twitter: 'nirmalrizal',
-      facebook: 'nirmal.rijal.16'
+      github: "nirmalrizal53",
+      twitter: "nirmalrizal",
+      facebook: "nirmal.rijal.16"
     }
   },
   // manishmarahatta
   {
-    fullName: 'Manish Marahatta',
-    discordUsername: 'manishmarahatta#6214',
-    about: 'developer, emacs and open source addict, craze for AI',
+    fullName: "Manish Marahatta",
+    discordUsername: "manishmarahatta#6214",
+    about: "developer, emacs and open source addict, craze for AI",
     links: {
-      github: 'manishmarahatta',
-      twitter: 'mmarahatta',
-      facebook: 'mmarahatta'
+      github: "manishmarahatta",
+      twitter: "mmarahatta",
+      facebook: "mmarahatta"
     }
   },
   // nabinsademba
   {
-    fullName: 'Nabin Sademba',
-    discordUsername: 'Monk34#5561',
-    about: 'nerd,geek :P',
+    fullName: "Nabin Sademba",
+    discordUsername: "Monk34#5561",
+    about: "nerd,geek :P",
     links: {
-      github: 'nabins13',
-      twitter: 'nabin sademba',
-      facebook: 'नविन सादेम्बा लिम्बु'
+      github: "nabins13",
+      twitter: "nabin sademba",
+      facebook: "नविन सादेम्बा लिम्बु"
     }
   },
   // amitchaudhary
   {
-    fullName: 'Amit Chaudhary',
-    discordUsername: 'amitness#3292',
-    about: 'I love playing with data.',
+    fullName: "Amit Chaudhary",
+    discordUsername: "amitness#3292",
+    about: "I love playing with data.",
     links: {
-      github: 'amitness',
-      facebook: 'amitify'
+      github: "amitness",
+      facebook: "amitify"
     }
   },
   //cdrrazan
   {
-    fullName: 'Rajan Bhattarai',
-    discordUsername: 'cdrrazan#3388',
-    about: 'ROR, Atom and Open Source Promoter',
+    fullName: "Rajan Bhattarai",
+    discordUsername: "cdrrazan#3388",
+    about: "ROR, Atom and Open Source Promoter",
     links: {
-      facebook: 'cdrrazan',
-      twitter: 'cdrrazan',
-      github: 'cdrrazan'
+      facebook: "cdrrazan",
+      twitter: "cdrrazan",
+      github: "cdrrazan"
     }
   },
   // Animesh Risal
   {
-    fullName: 'Animesh Risal',
-    discordUsername: 'TenToasts#8794',
-    about: 'Machine Learning, NLP, open education',
+    fullName: "Animesh Risal",
+    discordUsername: "TenToasts#8794",
+    about: "Machine Learning, NLP, open education",
     links: {
-      github: 'toastypixels',
-      twitter: 'TenToasts',
-      facebook: 'TenToasts'
+      github: "toastypixels",
+      twitter: "TenToasts",
+      facebook: "TenToasts"
     }
   },
   // Sabin Nepal
   {
-    fullName: 'Sabin Nepal',
-    discordUsername: 'neymarsabin#2179',
-    about: 'I am a CSIT student and I like to collaborate.',
+    fullName: "Sabin Nepal",
+    discordUsername: "neymarsabin#2179",
+    about: "I am a CSIT student and I like to collaborate.",
     links: {
-      github: 'neymarsabin'
+      github: "neymarsabin"
     }
   },
   // Pradip Khadka
   {
-    fullName: 'Pradip Khadka',
-    discordUsername: 'pradyp.me#2193',
-    about: 'Developer, EDM Lover, Lifelong Learner',
+    fullName: "Pradip Khadka",
+    discordUsername: "pradyp.me#2193",
+    about: "Developer, EDM Lover, Lifelong Learner",
     links: {
-      github: 'coderpradp',
-      twitter: 'pradpkhadka',
-      facebook: 'iampradp'
+      github: "coderpradp",
+      twitter: "pradpkhadka",
+      facebook: "iampradp"
     }
   },
   // Pratik Chaudhary
   {
-    fullName: 'Pratik Chaudhary',
-    discordUsername: 'AbsoluteZero#0296',
-    about: 'Hacks FP',
+    fullName: "Pratik Chaudhary",
+    discordUsername: "AbsoluteZero#0296",
+    about: "Hacks FP",
     links: {
-      github: 'AbsoluteZero273',
-      twitter: 'CodeZero273'
+      github: "AbsoluteZero273",
+      twitter: "CodeZero273"
     }
   },
   // Sujan Rijal
   {
-    fullName: 'Sujan Rijal',
-    discordUsername: 'Sujanrijal16#8997',
-    about: 'IT student and here to contribute for new experience.',
+    fullName: "Sujan Rijal",
+    discordUsername: "Sujanrijal16#8997",
+    about: "IT student and here to contribute for new experience.",
     links: {
-      github: 'Sujanrijal16',
+      github: "Sujanrijal16"
     }
   },
   // Kiran Shahi
   {
-    fullName: 'Kiran Shahi',
-    discordUsername: 'Kiran Shahi#0366',
-    about: 'Student | Developer | .NET',
+    fullName: "Kiran Shahi",
+    discordUsername: "Kiran Shahi#0366",
+    about: "Student | Developer | .NET",
     links: {
-      github: 'kiranshahi',
-      twitter: 'itskirans',
-      facebook: 'itskirans',
+      github: "kiranshahi",
+      twitter: "itskirans",
+      facebook: "itskirans"
     }
   },
   // Pramesh Bajracharya
   {
-    fullName: 'Pramesh Bajracharya',
-    discordUsername: 'pe.messh#5758',
-    about: 'Computer Science Enginner | Tech Enthusiast ',
+    fullName: "Pramesh Bajracharya",
+    discordUsername: "pe.messh#5758",
+    about: "Computer Science Enginner | Tech Enthusiast ",
     links: {
-      github: 'Suzal3579',
-      twitter: 'suzal33',
-      facebook: 'pe.messh',
-      instagram: 'pe.messh'
+      github: "Suzal3579",
+      twitter: "suzal33",
+      facebook: "pe.messh",
+      instagram: "pe.messh"
     }
   },
   {
-    fullName: 'Jenish Shrestha',
-    discordUsername: 'jenish1231#4002',
-    about: 'Student',
+    fullName: "Jenish Shrestha",
+    discordUsername: "jenish1231#4002",
+    about: "Student",
     links: {
-      github: 'jenish1231'
+      github: "jenish1231"
     }
   },
   //Ankit Chhetri
   {
-    fullName: 'Ankit Chhetri',
-    discordUsername: 'bhukampa#2489',
-    about: 'Developer | Student | Snake Charmer',
+    fullName: "Ankit Chhetri",
+    discordUsername: "bhukampa#2489",
+    about: "Developer | Student | Snake Charmer",
     links: {
-      github: 'ankitch',
-      twitter: 'ankit_cheetri'
+      github: "ankitch",
+      twitter: "ankit_cheetri"
     }
   },
   // Nischal Lal Shrestha
   {
-    fullName: 'Nischal Lal Shrestha',
-    discordUsername: 'Nischal#8165',
-    about: 'Programmer|Inventor|Django|Python|Learner',
+    fullName: "Nischal Lal Shrestha",
+    discordUsername: "Nischal#8165",
+    about: "Programmer|Inventor|Django|Python|Learner",
     links: {
-      github: 'theonlyNischal',
-      twitter: 'theonlyNischal',
-      facebook: 'theonlyNischal',
+      github: "theonlyNischal",
+      twitter: "theonlyNischal",
+      facebook: "theonlyNischal"
     }
   },
- {
-    fullName: 'Bishal Thapa',
-    discordUsername: 'thebsaal#0147',
-    about: 'Developer|Designer|Mentor|Learner',
+  {
+    fullName: "Bishal Thapa",
+    discordUsername: "thebsaal#0147",
+    about: "Developer|Designer|Mentor|Learner",
     links: {
-      github: 'thebsaal',
-      twitter: 'thebsaal',
-      facebook: 'thebsaal',
+      github: "thebsaal",
+      twitter: "thebsaal",
+      facebook: "thebsaal"
     }
- },
- // Ashish Tiwari
- {
-    fullName: 'Ashish Tiwari',
-    discordUsername: 'megamindAT',
-    about: 'Antarctica is melting!',
+  },
+  // Ashish Tiwari
+  {
+    fullName: "Ashish Tiwari",
+    discordUsername: "megamindAT",
+    about: "Antarctica is melting!",
     links: {
-      github: 'megamind98',
-      twitter: 'megamindAT',
-      facebook: 'megamindAT',
+      github: "megamind98",
+      twitter: "megamindAT",
+      facebook: "megamindAT"
     }
   },
   // Subesh Bhandari
   {
-    fullName: 'Subesh Bhandari',
-    discordUsername: 'Subeshb1#3437',
-    about: 'Programmer/Student',
+    fullName: "Subesh Bhandari",
+    discordUsername: "Subeshb1#3437",
+    about: "Programmer/Student",
     links: {
-      github: 'subeshb1',
-      twitter: 'subeshb1'
+      github: "subeshb1",
+      twitter: "subeshb1"
     }
   },
   // Prabin Parajuli
   {
-    fullName: 'Prabin Parajuli',
-    discordUsername: 'prabin-parajuli#8986',
-    about: '**Loves** Binary world.',
+    fullName: "Prabin Parajuli",
+    discordUsername: "prabin-parajuli#8986",
+    about: "**Loves** Binary world.",
     links: {
-      github: 'prabinzz',
-      twitter: 'prabinparajuli',
-      facebook: 'pra.bin.35'
+      github: "prabinzz",
+      twitter: "prabinparajuli",
+      facebook: "pra.bin.35"
+    }
+  },
+  // Manjil Tamang
+  {
+    fullName: "Manjil Tamang",
+    discordUsername: "MJL#1862",
+    about: "Full stack developer",
+    links: {
+      github: "manjillama", // required
+      twitter: "lamamanjil"
     }
   }
   // add here
-]
+];
 /* ====================================================================== */
-const membersContainer = document.querySelector('.members')
-let membersElms = '' // members elements
+const membersContainer = document.querySelector(".members");
+let membersElms = ""; // members elements
 
 // Generate html of members data
 
 members.forEach(member => {
-  let memElm =
-    `<div class='member'>
+  let memElm = `<div class='member'>
       <img class='member-image'
         src='https://github.com/${member.links.github}.png?size=100',
         alt='${member.fullName}'
@@ -283,23 +292,23 @@ members.forEach(member => {
       <h2 class='member-username'>${member.fullName}</h2> <p title='Discord username' class='member-discord-user'>@${member.discordUsername} </p><hr>
       <p>${member.about}</p><hr>
       <div class='member-links'>
-    `
+    `;
 
   if (member.links.github) {
-    memElm += `<a href='https://github.com/${member.links.github}'><i class='fa fa-github'></i></a>`
+    memElm += `<a href='https://github.com/${member.links.github}'><i class='fa fa-github'></i></a>`;
   }
 
   if (member.links.twitter) {
-    memElm += `<a href='https://twitter.com/${member.links.twitter}'><i class='fa fa-twitter'></i></a>`
+    memElm += `<a href='https://twitter.com/${member.links.twitter}'><i class='fa fa-twitter'></i></a>`;
   }
 
   if (member.links.facebook) {
-    memElm += `<a href='https://facebook.com/${member.links.facebook}'><i class='fa fa-facebook'></i></a>`
+    memElm += `<a href='https://facebook.com/${member.links.facebook}'><i class='fa fa-facebook'></i></a>`;
   }
 
-  memElm += `</div></div>`
+  memElm += `</div></div>`;
 
-  membersElms += memElm
-})
+  membersElms += memElm;
+});
 
-membersContainer.innerHTML = membersElms
+membersContainer.innerHTML = membersElms;
